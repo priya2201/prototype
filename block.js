62fc6707dff773c2124cd6546b5289365a3f6f2e
priyamurpani <priyamurpani@nimapinfotech.com>
2023-01-06 15:58:58 +0530

    done

---
+++ block.js
@@ -0,0 +1,49 @@
+// var b=20;
+// let c=22;
+// {
+//     var a=10;
+//     let  b=10;
+//     const c=30;
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
+// var a=20;
+// let b=20;
+// let c=22;
+// function hie() {
+//     var b=10;
+//     console.log(b);
+   
+// }
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// hie();
+// var b=100;
+// const a=10;
+// console.log(a);
+// {
+//    // var b=10;
+//     const a=20;
+//     {
+//         const a =30;
+//         function X(){
+//             console.log(b);
+//         }
+//         X();
+//     }
+// }
+
+var k=100;
+function m(){
+    console.log(k);
+    var k=80;
+}
+m();
